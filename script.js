659ab06f727811d3dfa3d77b0c3d8199edc54939
) <26818084+Tomko1337@users.noreply.github.com>
2023-03-16 01:06:33 +0100

    update
    -rewrited the code
    -added some options

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const menuContainer = document.querySelector('.menu-container');
 menuContainer.addEventListener('click', () => {
 	menuContainer.classList.toggle('open');
 });
+if (window.location.href.indexOf(".html") > -1) {
+    var newUrl = window.location.href.replace(".html", "");
+    window.history.replaceState(null, null, newUrl);
+  }
