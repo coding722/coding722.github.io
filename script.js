8a4c429e4a2deacd3e0ca0e3c0bc94f2f2435b71
) <26818084+Tomko1337@users.noreply.github.com>
2023-03-16 01:18:08 +0100

    fixes
    -cursor issues

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const menuContainer = document.querySelector('.menu-container');
 
 menuContainer.addEventListener('click', () => {
-	menuContainer.classList.toggle('open');
+    menuContainer.classList.toggle('open');
 });
 if (window.location.href.indexOf(".html") > -1) {
     var newUrl = window.location.href.replace(".html", "");
     window.history.replaceState(null, null, newUrl);
-  }
+}
